5074d0735c218470a4182ce979313e9243d9468c
Akhilvannery <akhilvannery@gmail.com>
2020-10-09 14:01:22 +0530

    My first commit

---
+++ MyFirst.java
@@ -0,0 +1,17 @@
+package myfirst;
+import java.util.*;
+
+public class MyFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter base:");
+		int a,b,c;
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("the sum is "+c);
+	}
+
+}
